2fc07b930a79a420348d14aabf7f9551f31a74f2
Mito Youhei <mitoyouhei@proton.me>
2024-07-25 19:30:23 +0800

    enable create date

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,16 +3,40 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import { HiMiniPlus, HiMiniUserCircle } from "react-icons/hi2";
 import { addNote, deleteFolder } from "../websocket";
 import { buildTree } from "../utils";
+import { DateTime } from "luxon";
+import { useState } from "react";
 
 const defaultNoteTitle = "Untitled";
 
 const NavItem = ({ folder, isActive }) => {
   const navigate = useNavigate();
+  const [showMenu, setShowMenu] = useState(false);
 
-  const handleDeleteFolder = async (id) => {
+  // 解析日期字符串
+  const date = DateTime.fromISO(folder.createdAt);
+
+  const handleDeleteFolder = async (e, id) => {
+    e.preventDefault();
     await deleteFolder(id);
     navigate("/");
   };
+  const toggleMenu = (e) => {
+    e.preventDefault();
+    setShowMenu(!showMenu);
+  };
+  const FunctionMenu = () =>
+    showMenu ? (
+      <ul className="dropdown-menu d-block mt-4">
+        <li>
+          <button
+            className="dropdown-item"
+            onClick={(e) => handleDeleteFolder(e, folder._id)}
+          >
+            Delete
+          </button>
+        </li>
+      </ul>
+    ) : null;
   return (
     <Link
       className={`list-group-item list-group-item-action ${
@@ -23,10 +47,43 @@ const NavItem = ({ folder, isActive }) => {
       {/* <Link to={`/note/${folder._id}`} className="btn btn-primary">
       
     </Link> */}
-      {folder.name ? folder.name : defaultNoteTitle}
+      <div className="title-row">
+        <b>{folder.name ? folder.name : defaultNoteTitle}</b>
+
+        <div className="dropdown">
+          <span
+            onClick={toggleMenu}
+            className="badge rounded-pill float-end mt-2"
+          >
+            <svg
+              role="graphics-symbol"
+              viewBox="0 0 13 3"
+              className="dots"
+              style={{
+                width: "14px",
+                height: "100%",
+                display: "block",
+                fill: "rgba(55, 53, 47, 0.45)",
+                flexShrink: "0",
+              }}
+            >
+              <g>
+                <path d="M3,1.5A1.5,1.5,0,1,1,1.5,0,1.5,1.5,0,0,1,3,1.5Z"></path>
+                <path d="M8,1.5A1.5,1.5,0,1,1,6.5,0,1.5,1.5,0,0,1,8,1.5Z"></path>
+                <path d="M13,1.5A1.5,1.5,0,1,1,11.5,0,1.5,1.5,0,0,1,13,1.5Z"></path>
+              </g>
+            </svg>
+          </span>
+          <FunctionMenu />
+        </div>
+      </div>
+      <small className="fw-lighter" style={{ fontSize: ".675em" }}>
+        {date.toFormat("MM/dd")}
+      </small>
+      {/* <small>{date.toFormat("yyyy-MM-dd HH:mm:ss")}</small> */}
       {/* <button
         className="btn btn-danger btn-sm float-end"
-        onClick={() => handleDeleteFolder(folder._id)}
+        onClick={(e) => handleDeleteFolder(e, folder._id)}
       >
         Delete
       </button> */}
